e347965e76ef179ac35229ab4a9e3ae95a465283
pavanbawdane <bawdane3@gmail.com>
2015-11-22 02:13:53 -0600

    Updates in the UI - Login Activity and Register

--- app/src/main/java/edu/uta/gigme/RegisterActivity.java
+++ app/src/main/java/edu/uta/gigme/RegisterActivity.java
@@ -1,19 +1,50 @@
 package edu.uta.gigme;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
 
-public class RegisterActivity extends AppCompatActivity {
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
+public class RegisterActivity extends AppCompatActivity implements View.OnClickListener {
+    EditText mEtName, mEtEmail, mEtPhone, mEtDOB;
+    Button mBtnRegister;
+    private SimpleDateFormat dateFormat;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        //getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        mEtDOB = (EditText) findViewById(R.id.et_dob);
+        mBtnRegister = (Button) findViewById(R.id.btn_register);
+        mEtPhone = (EditText) findViewById(R.id.et_phone_number);
+        mBtnRegister.setOnClickListener(this);
+        mEtDOB.setOnClickListener(this);
     }
 
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.et_dob:
+                final Calendar calendar = Calendar.getInstance();
+                final int mYear = calendar.get(Calendar.YEAR);
+                final int mMonth = calendar.get(Calendar.MONTH);
+                final int mDay = calendar.get(Calendar.DAY_OF_MONTH);
+                DatePickerDialog datePickerDialog = new DatePickerDialog(
+                        RegisterActivity.this,
+                        new DatePickerDialog.OnDateSetListener() {
+                            @Override
+                            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                                mEtDOB.setText(monthOfYear + "-" + dayOfMonth + "-" + year);
+                                mEtPhone.requestFocus();
+                            }
+                        }, mYear, mMonth, mDay);
+                datePickerDialog.show();
+                break;
+        }
+    }
 }
