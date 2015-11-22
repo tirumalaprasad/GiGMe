d385a1abb4056bb99dac38b0f0887e65f51529ee
pavanbawdane <bawdane3@gmail.com>
2015-11-21 20:24:58 -0600

    Updates in the UI - Login Activity

--- app/src/main/java/edu/uta/gigme/LoginActivity.java
+++ app/src/main/java/edu/uta/gigme/LoginActivity.java
@@ -94,7 +94,7 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
 
         register.setOnClickListener(new View.OnClickListener(){
             public void onClick(View V){
-                startActivity(new Intent(LoginActivity.this, register.class));
+                startActivity(new Intent(LoginActivity.this, RegisterActivity.class));
             }
         });
     }
@@ -144,7 +144,7 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
 
 
     /**
-     * Attempts to sign in or register the account specified by the login form.
+     * Attempts to sign in or RegisterActivity the account specified by the login form.
      * If there are form errors (invalid email, missing fields, etc.), the
      * errors are presented and no actual login attempt is made.
      */
@@ -328,7 +328,7 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
                 }
             }
 
-            // TODO: register the new account here.
+            
             return true;
         }
 
--- app/src/main/java/edu/uta/gigme/RegisterActivity.java
+++ app/src/main/java/edu/uta/gigme/RegisterActivity.java
@@ -4,7 +4,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
-public class register extends AppCompatActivity {
+public class RegisterActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
