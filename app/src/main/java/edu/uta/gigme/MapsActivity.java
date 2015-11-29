60b23305be4929cab2994627170c58e85bf95aed
sidxlfc@gmail.com <sid.xlfc@gmail.com>
2015-11-29 02:08:46 -0600

    Event objects are stored in ArrayAdapter

--- app/src/main/java/edu/uta/gigme/MapsActivity.java
+++ app/src/main/java/edu/uta/gigme/MapsActivity.java
@@ -2,7 +2,6 @@ package edu.uta.gigme;
 
 import android.location.Address;
 import android.location.Geocoder;
-import android.os.AsyncTask;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -14,22 +13,12 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
-import com.google.android.gms.vision.barcode.Barcode;
 
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpResponse;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.params.BasicHttpParams;
-import org.apache.http.params.HttpConnectionParams;
-import org.apache.http.params.HttpParams;
-import org.apache.http.util.EntityUtils;
-import org.json.JSONObject;
 import android.support.v4.app.FragmentActivity;
 
 import java.util.List;
 import java.util.Locale;
+
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 {
@@ -51,11 +40,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
-
-        //map = ((MapFragment)getFragmentManager().findFragmentById(R.id.map)).getMap();
-        //private final LatLng eventCoordinates = new LatLng("");
-
-        //new ConvertAddressToCoordinates().execute();
 
         if (savedInstanceState == null)
         {
@@ -104,10 +88,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             if (fromLocationName != null && fromLocationName.size() > 0)
             {
                 Address a = fromLocationName.get(0);
-                /*Barcode.GeoPoint point = new Barcode.GeoPoint(
-                        (int) (a.getLatitude() * 1000),
-                        (int) (a.getLongitude() * 1000));*/
-                System.out.println("latlong : " + (double)a.getLatitude() + " " + (double)a.getLongitude());
+                System.out.println("latlong : " + (double) a.getLatitude() + " " + (double) a.getLongitude());
 
 
                 mMap = googleMap;
@@ -123,47 +104,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         catch (Exception e)
         {
             e.printStackTrace();
-        }
-    }
-
-    public class ConvertAddressToCoordinates extends AsyncTask<String, Void, String>
-    {
-        public ConvertAddressToCoordinates()
-        {
-
-        }
-
-        @Override
-        protected String doInBackground(String... params)
-        {
-            HttpParams httpRequestParams = new BasicHttpParams();
-            HttpConnectionParams.setConnectionTimeout(httpRequestParams, CONNECTION_TIMEOUT);
-            HttpConnectionParams.setSoTimeout(httpRequestParams,CONNECTION_TIMEOUT);
-
-            HttpClient client = new DefaultHttpClient(httpRequestParams);
-
-            HttpPost post = new HttpPost(SERVER_ADDRESS);
-
-            try
-            {
-                //client.execute(post);
-                HttpResponse httpResponse = client.execute(post);
-
-                HttpEntity entity = httpResponse.getEntity();
-                String result = EntityUtils.toString(entity);
-
-                System.out.println(result);
-
-                JSONObject jsonObject = new JSONObject(result);
-
-                System.out.println("blabla " + jsonObject.getString(""));
-            }
-
-            catch(Exception e)
-            {
-                e.printStackTrace();
-            }
-            return null;
         }
     }
 }
